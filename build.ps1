30a85ccc9531c01fd4a871c70e703288bb7ed292
Alena Hrynkevich <alena@appveyor.com>
2019-08-09 13:57:13 -0700

    Added build.ps1

---
+++ build.ps1
@@ -0,0 +1,5 @@
+appveyor version
+$ErrorActionPreference = "Stop"
+if(-not (Test-Path "$env:APPDATA\Windows Azure Powershell\AzureDataCollectionProfile.json")) { throw "AzureDataCollectionProfile.json does not exist."; }
+Get-Command Login-AzureRmAccount
+Get-Command Get-AzureRmApiManagementBackend
